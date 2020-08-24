de0334369a05e936e9e21501dfbda35f79392562
ikcilrep <frajerzycki@protonmail.com>
2020-08-25 00:56:45 +0200

    Start using the Login component

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Header } from 'react-native-elements';
+import Login from './Components/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <Header
+        placement='left'
+        centerComponent={{ text: 'MYSTIK', style: { color: '#fff' } }}
+      />
+      <Login />
+
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
