a5ea6623615bb6b7de43203831f0715de79c0514
ikcilrep <frajerzycki@protonmail.com>
2020-08-25 00:56:37 +0200

    Create a Login component

---
+++ Components/Login.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
+
+const Login = () => (
+    <View style={styles.container}>
+        <Input
+            placeholder='Username'
+            leftIcon={{ type: 'font-awesome', name: 'user' }}
+            label='Username'
+        />
+        <Input
+            placeholder='Password'
+            leftIcon={{ type: 'font-awesome', name: 'lock' }}
+            secureTextEntry={true}
+            label='Password'
+        />
+        <Button
+            icon={{
+                name: "check",
+                size: 15,
+                color: "black"
+            }}
+            title="Log in"
+        />
+    </View>
+);
+
+export default Login;
