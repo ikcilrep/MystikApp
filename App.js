b491cf6844b9bd3000f14050c3c8950dd3790b23
ikcilrep <frajerzycki@protonmail.com>
2020-08-25 01:56:23 +0200

    Create a tab navigation

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Header } from 'react-native-elements';
 import Login from './Components/Login';
+import Register from './Components/Register';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -10,7 +17,38 @@ export default function App() {
         placement='left'
         centerComponent={{ text: 'MYSTIK', style: { color: '#fff' } }}
       />
-      <Login />
+
+
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch (route.name) {
+              case 'Login':
+                iconName = 'check';
+                break;
+              case 'Register':
+                iconName = 'plus';
+                break;
+              default:
+                break;
+
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Login" component={Login} />
+          <Tab.Screen name="Register" component={Register} />
+
+        </Tab.Navigator>
+
+      </NavigationContainer>
 
       <StatusBar style="auto" />
     </>
